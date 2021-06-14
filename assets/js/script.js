c4c67f3df6c870050f7dfb966e1c1597c50bfb74
algmoreno <alg.moreno00@gmail.com>
2021-06-14 14:48:39 -0700

    retrieving artist name and top ten

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+var searchBar = document.querySelector("#searchBar");
+var searchButton = document.querySelector("#searchButton");
+var imageArea= document.querySelector("#imageArea"); 
+var middleColumn = document.querySelector("#middleColumn");
+var topTenLi; 
+
+var fetchMusic = function(artist) {
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${artist}`)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data){
+        displayArtist(data); 
+    })
+}
+
+var searchArtist = function () {
+    userSearch = searchBar.value; 
+    fetchMusic(userSearch); 
+}
+
+var displayArtist = function (artistData) {
+    console.log(artistData); 
+
+    var artistNameBox = document.createElement("div");
+    artistNameBox.className = "artist-name-box";
+    
+    var artist = document.createElement("h2"); 
+    artist.textContent = artistData.data[0].artist.name; 
+    artist.className= "artist-name"; 
+    artistNameBox.append(artist);
+
+    var topTenBox = document.createElement("div");
+    topTenBox.className = "top-ten-box";
+
+    var topTenBoxUl = document.createElement("ul");
+
+    for (var i = 0; i < 10; i++) {
+    topTenLi = document.createElement("li");
+    topTenLi.textContent = 
+    topTenLi.className = "top-ten-list";
+
+    }
+
+
+    middleColumn.append(imageArea);
+    middleColumn.append(artistNameBox);
+    middleColumn.append(topTenBox)
+}
+
+searchButton.addEventListener("click", searchArtist); 
